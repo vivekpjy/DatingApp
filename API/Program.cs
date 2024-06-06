27cba9e6ce9b60dff4c6e058e10c24051df426f2
vivekpjy <vivekpoojary5@gmail.com>
2024-06-06 23:31:41 +0530

    Section 2 Angular Settup and SSL certificate

--- API/Program.cs
+++ API/Program.cs
@@ -13,14 +13,18 @@ builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//to connect with frontEnd 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllers();
 
